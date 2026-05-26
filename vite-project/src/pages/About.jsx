0e1959ee2941b4d49ea536da7fa616aa67afe22c
Gulnur <taskin.gulnur@gmail.com>
2026-05-26 10:44:31 +0200

    about

--- vite-project/src/pages/About.jsx
+++ vite-project/src/pages/About.jsx
@@ -15,33 +15,40 @@ export default function About() {
       <Banner title="" image={bannerAbout} className="banner-about" />
 
       <div className="about-collapses">
-        <Collapse
-          title="Fiabilité"
+       <Collapse title="Fiabilité">
+  <p>
+    Les annonces postées sur Kasa garantissent une fiabilité totale.
+    Les photos sont conformes aux logements, et toutes les informations
+    sont régulièrement vérifiées par nos équipes.
+  </p>
+</Collapse>
 
-        >
-          <p>Les annonces postées sur Kasa garantissent une fiabilité totale...</p>
-        </Collapse>
+<Collapse title="Respect">
+  <p>
+    La bienveillance fait partie des valeurs fondatrices de Kasa.
+    Tout comportement discriminatoire ou de perturbation du voisinage
+    entraînera une exclusion de notre plateforme.
+  </p>
+</Collapse>
 
-        <Collapse
-          title="Respect"
+<Collapse title="Service">
+  <p>
+    La qualité du service est au cœur de notre engagement chez Kasa.
+    Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes
+    ou nos locataires, soit empreinte de respect et de bienveillance.
+  </p>
+</Collapse>
 
-        >
-          <p>La bienveillance fait partie des valeurs fondatrices de Kasa...</p>
-        </Collapse>
-
-        <Collapse
-          title="Service"
-
-        >
-          <p>La qualité du service est au cœur de nos priorités...</p>
-        </Collapse>
-
-        <Collapse
-          title="Sécurité"
-
-        >
-          <p>La sécurité est la priorité de Kasa...</p>
-        </Collapse>
+<Collapse title="Sécurité">
+  <p>
+    La sécurité est la priorité de Kasa.
+    Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+    correspond aux critères de sécurité établis par nos services.
+    En laissant une note aussi bien à l'hôte qu'au locataire,
+    cela permet à nos équipes de vérifier que les standards sont bien respectés.
+    Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
+  </p>
+</Collapse>
       </div>
     </div>
   );
